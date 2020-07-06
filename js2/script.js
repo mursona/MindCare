7e1e80e5c3b2033e6d418d758cc66d0732a95e1e
Mahabuba Akter Mursona <48563123+mursona@users.noreply.github.com>
2020-07-06 10:26:16 +0600

    Add files via upload

---
+++ js2/script.js
@@ -0,0 +1,2 @@
+jQuery("#wowslider-container")
+.wowSlider({effect:"rotate",prev:"",next:"",duration:10*10,delay:10*10,width:400,height:100,autoPlay:true,stopOnHover:false,loop:false,bullets:true,caption:true,captionEffect:"slide",controls:true,logo:"",images:0});
